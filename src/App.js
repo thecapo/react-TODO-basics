4fbe5c687ce0c6b5aa2406f7c9b5b6d4e286d1de
thecapo <the7thcapo@gmail.com>
2018-04-24 23:14:29 +1000

    can add todo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,46 @@ import './App.css';
 import Titles from './components/Titles';
 
 class App extends Component {
+  state = {
+    todos: [],
+    text: ''
+  }
+
+  handleSubmit = this.handleSubmit.bind(this);
+  handleChange = this.handleChange.bind(this);
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, this.state.text],
+      text: ''
+    })
+  }
+
   render() {
+    let todos = this.state.todos;
     return (
       <div>
         <Titles /> 
+        <form onSubmit={this.handleSubmit}>
+          <input  type="text" 
+                  placeholder="Enter TODO"
+                  value={this.state.text}
+                  onChange={this.handleChange}
+          />
+          <button>Add</button>
+        </form>
+        {todos.map((todo, index) => 
+          <li key={index}>
+            {todo}
+          </li>
+        )}
       </div>
     );
   }
