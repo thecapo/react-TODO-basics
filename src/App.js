f799890613a907fb0e7b003258da89304920d8cc
thecapo <the7thcapo@gmail.com>
2018-04-24 23:07:32 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Titles from './components/Titles';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Titles /> 
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <p>TODO</p>
+      </div>
+    )
+  }
+}
+
+export default Titles;
